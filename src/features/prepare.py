946689d1e4be69594aa8e16492b6e3ca788d63ee
renatofnc <104675228+renatofnc@users.noreply.github.com>
2023-01-23 18:19:32 +0000

    Change to a simpler prepare stage

--- src/features/prepare.py
+++ src/features/prepare.py
@@ -1,33 +1,34 @@
 import os
 import yaml
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
 # read params
 
-params = yaml.safe_load(open('params.yaml'))['prepare']
+#params = yaml.safe_load(open('params.yaml'))['prepare']
 
-features = params['features'] 
+#features = params['features'] 
 
 # create folder to save file
 
 data_path = os.path.join('data', 'prepared')
-os.makedirs(data_path, exist_ok= True)
+os.makedirs(data_path, exist_ok= True) 
 
 
+#TODO  hier sollte eine Methode entstehen, um alle oder bestimmte relevante Ordner zu lesen 
+# und Daten aus Experimente und/oder remote/storage und/oder Wrapper zu fetchen
+data = pd.read_pickle("./data/raw/remote_storage/challenges_2.pkl")
 
-data_train = data_path(subset = 'train', features= 'features')
-data_test = data_path(subset = 'test', features= 'features')
+#data_train = data_path(subset = 'train', features= 'features')
+#data_test = data_path(subset = 'test', features= 'features')
 
-def data_to_csv(split_name, data):
-    """TODO docstring needed"""
-    df = pd.DataFrame([data.data, data.target.tolist()]).T
-    df.columns = ['challengee_lat', 'challengee_lon', 'witness_lat', 'witness_lat', 'signal', 'snr', 'datarate', 'time']
+def splitter(inputdata):
+    train, test = train_test_split(
+        data,
+        test_size=0.3,
+        random_state=42
+    )
+    train.to_csv(data_path + "/train.csv", sep=",", index=False, encoding="utf-8")
+    test.to_csv(data_path + "/test.csv", sep=",", index=False, encoding="utf-8")
 
-    df_target_names = pd.DataFrame(data.target_names)
-    df_target_names.columns = ['distance']
-
-    out = pd.merge(df, df_target_names, left_on='target', right_index=True)
-    out.to_csv(os.path.join(data_path, split_name+".csv"))
-    
-data_to_csv('train', data_train)   
-data_to_csv('test', data_test) 
+if __name__ =="__main__":
+    splitter(data)
