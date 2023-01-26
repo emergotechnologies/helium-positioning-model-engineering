c36d05ea69529645b746280aabc059783f7bb259
renatofnc <104675228+renatofnc@users.noreply.github.com>
2023-01-26 00:12:35 +0000

    refactoring

--- src/features/train.py
+++ src/features/train.py
@@ -13,26 +13,18 @@ def train(config_path):
     #config = read_params(config_path)
     test_data_path = params["split_data"]["test_path"]
     train_data_path = params["split_data"]["train_path"]
-    random_state = params["base"]["random_state"]
     model_dir = params["model_dir"]
     target = [params["base"]["target_col"]]
-
     train = pd.read_csv(train_data_path, sep=",")
     print("Train Size", train.shape[0])
     test = pd.read_csv(test_data_path, sep=",")
     print("Test Size", test.shape[0])
-    
 
     train_y = train[target]
-
-
     train_x = train.drop(target, axis=1)
-    
 
     # Build linear regression model
-    
     model = LinearRegression().fit(train_x, train_y)
-   
     os.makedirs(model_dir, exist_ok=True)
     model_path = os.path.join(model_dir, "model.joblib")
     print("Export Joblib to ", model_path)
